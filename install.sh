b44445acb0f9a09be5fc771d22c30d8cb5ff5a8f
John David Johnson II <johnsonjo4531@gmail.com>
2024-03-08 11:38:00 -0700

    Updated install script to work better

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Define variables
 SCRIPT_TO_INSTALL="cpstarter"                            # name of your script, adjacent to this install script
-INSTALL_SCRIPT_DIR="$(dirname "$0")"                 # directory of the current install script
+INSTALL_SCRIPT_DIR="$(dirname `realpath "$0"`)"                 # directory of the current install script
 ABSOLUTE_ADJACENT_FILE="$INSTALL_SCRIPT_DIR/$SCRIPT_TO_INSTALL" # absolute path of SCRIPT_TO_INSTALL
 INSTALL_DIR="$HOME/.local/bin"                           # user-specific directory to install the symbolic link
-PATH_ADDITION="$HOME/.local/bin"                         # path addition for user level access (optional)
 # Check if the SCRIPT_TO_INSTALL exists in the same directory as this install script
 if [ ! -e "$ABSOLUTE_ADJACENT_FILE" ]; then
 	echo "Error: $SCRIPT_TO_INSTALL not found in the current directory!"
@@ -24,7 +23,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Add path addition if provided and not already in PATH
-if [[ ":$PATH:" != *":$PATH_ADDITION:"* ]] && [ -n "$PATH_ADDITION" ]; then
+if [[ ":$PATH:" != *":$INSTALL_DIR:"* ]] && [ -n "$INSTALL_DIR" ]; then
 	echo 'WARNING: 
 	Your PATH variable does not include your $HOME/.local/bin in your .bashrc, or .zshrc, etc.
 	You'"'"'ll need to provide it in your .bashrc or similar file:
@@ -40,5 +39,5 @@ you will have successfully installed $SCRIPT_TO_INSTALL to $INSTALL_DIR.
 After that please restart the terminal or run 'source ~/.bashrc' or similar file 
 for changes to take effect."'
 else
-	echo "Successfully installed $SCRIPT_TO_INSTALL to $INSTALL_DIR. Please restart the terminal or run 'source ~/.bashrc' for changes to take effect."
+	echo "Successfully installed $SCRIPT_TO_INSTALL to $INSTALL_DIR."
 fi
