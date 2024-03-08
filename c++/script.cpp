b89097e3f037f05c4ecb991e498c80fb511449a0
John David Johnson II <johnsonjo4531@gmail.com>
2024-03-07 21:18:05 -0700

    Updating C++

--- c++/script.cpp
+++ c++/script.cpp
@@ -1,12 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main () {
-  int n;
-  int c [n];
-  cin >> n;
-  for (int i = 0; i < n; ++i) {
-    cin >> c[i];
-  } 
+  cout << "foo" << endl;
+  // your code here
   return 0;
 }
